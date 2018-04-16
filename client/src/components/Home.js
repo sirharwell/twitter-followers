ae302fa5fd091b7f0057154ee2687831f7de197f
Ian Harwell <sirharwell@gmail.com>
2018-04-16 10:54:51 -0600

    StaggeredMotion

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -19,7 +19,30 @@ class Home extends Component {
   handleMouseMove = ({ pageX: x, pageY: y }) => {
       this.setState({ x, y })
   }
+
+  getStyles = (prevStyles) => {
+    const endValue = prevStyles.map((_,i) => {
+      return i === 0
+      ? this.state
+      : {
+        x: spring(prevStyles[i-1].x, presets.gentle),
+        y: spring(prevStyles[i-1].y, presets.gentle), 
+      }
+    })
+    return endValue
+  }
   render() {
+    const { followers } = this.state;
+    if (followers.length) {
+      return (
+        <StaggeredMotion
+          defaultStyles={
+            followers.map(() => ({x: 0, y: 0 }))}
+            styles={this.getStyles}
+        >{ avatars =>
+
+        }</StaggeredMotion>)
+    }
     return (
       <Header as='h1' textAlign='center'>Home Component</Header>
     );
