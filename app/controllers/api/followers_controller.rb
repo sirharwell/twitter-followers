5dccbeff8db0c1c1f97e2d359ec3eeb267ae5a37
Ian Harwell <sirharwell@gmail.com>
2018-04-16 10:45:45 -0600

    home

--- app/controllers/api/followers_controller.rb
+++ app/controllers/api/followers_controller.rb
@@ -1,4 +1,16 @@
 class Api::FollowersController < ApplicationController
+  before_action :client
+
   def index
+    followers = @client.followers.take(10).collect do |follower|
+  end
+
+  private
+  def client
+    @client = Twitter::REST::Client.new do |c|
+      c.consumer_key = ENV['CONSUMER_KEY']
+      c.consumer_secret = ENV['CONSUMER_SECRET']
+      c.access_token = ENV['ACCESS_TOKEN']
+      c.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
   end
 end
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import axios from 'axios'
+import {
+  StaggeredMotion,
+  spring,
+  presets,
+} from 'react-motion';
+
 
 class Home extends Component {
+
+  componentDidMount() {
+    window.addEventListener('mousemove', this.handleMouseMove)
+    axios.get('/api/followers')
+      .then( ({data}) => this.setState({ followers: data }) )
+  }
+
+  handleMouseMove = ({ pageX: x, pageY: y }) => {
+      this.setState({ x, y })
+  }
   render() {
     return (
       <Header as='h1' textAlign='center'>Home Component</Header>
